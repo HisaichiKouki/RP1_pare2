622c40c82c9c84d19a3299db5fb6af141e66b4ea
ヒサイチ_コウキ <k023g0068@g.neec.ac.jp>
2024-05-31 21:09:20 +0900

    歓声システム仮実装

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,19 +6,23 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
      Rigidbody2D playerRigidbody;
+    private bool ishold;
+    GameObject holdObj;
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
+        ishold = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
-
+        Hold();
     }
 
+    
     void Move()
     {
 
@@ -29,5 +33,25 @@ public class Player : MonoBehaviour
         Vector2 velocity = input * moveSpeed;
         Debug.Log("vel=" + velocity);
         playerRigidbody.velocity = velocity;
+    }
+
+    void Hold()
+    {
+        //if(holdObj == null) { return; }
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("isHold");
+            ishold = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        holdObj=collision.gameObject;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        holdObj = null;
     }
 }
