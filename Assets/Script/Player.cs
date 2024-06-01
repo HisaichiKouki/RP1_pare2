f5c2d8d9877ede807dd2815a38877f7554283c23
ヒサイチ_コウキ <k023g0068@g.neec.ac.jp>
2024-06-01 15:53:03 +0900

    playerのアニメーションを実装

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
+
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed;
-     Rigidbody2D playerRigidbody;
-    private bool ishold;
+    [SerializeField] private Vector2 moveSpeed;
+    Rigidbody2D playerRigidbody;
+    private Vector2 input;
+    private GameObject playerAnimeObj;
+    private Animator playerAnime = null;
+    private bool isYadoHold;
     GameObject holdObj;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
-        ishold = false;
+        isYadoHold = false;
+        playerAnimeObj = transform.GetChild(0).gameObject;
+        playerAnime=playerAnimeObj.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -20,19 +28,28 @@ public class Player : MonoBehaviour
     {
         Move();
         Hold();
+        SetAnimator();
     }
 
-    
+
     void Move()
     {
 
-        Vector2 input;
+       
         input.x = Input.GetAxis("Horizontal");
         input.y = Input.GetAxis("Vertical");
 
-        Vector2 velocity = input * moveSpeed;
+        Vector2 velocity = input* moveSpeed;
         Debug.Log("vel=" + velocity);
         playerRigidbody.velocity = velocity;
+        if (velocity.x > 0)
+        {
+            playerAnimeObj.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if (velocity.x < 0)
+        {
+            playerAnimeObj.GetComponent<SpriteRenderer>().flipX = true;
+        }
     }
 
     void Hold()
@@ -40,14 +57,21 @@ public class Player : MonoBehaviour
         //if(holdObj == null) { return; }
         if (Input.GetButtonDown("Jump"))
         {
-            Debug.Log("isHold");
-            ishold = true;
+            Debug.Log("isYadoHold");
+            isYadoHold =!isYadoHold;
         }
     }
 
+    void SetAnimator()
+    {
+        playerAnime.SetFloat("speed", input.magnitude);
+        playerAnime.SetBool("isYadoHold", isYadoHold);
+
+        
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        holdObj=collision.gameObject;
+        holdObj = collision.gameObject;
     }
 
     private void OnCollisionExit(Collision collision)
