f216870eaac4404bb0d0eafe61443a905e57dd36
ヒサイチ_コウキ <k023g0068@g.neec.ac.jp>
2024-06-01 17:00:30 +0900

    持ってる時ヤドに経験値がたまるシステム(カウントだけ)実装

--- Assets/YadoScript.cs
+++ Assets/YadoScript.cs
@@ -8,11 +8,15 @@ public class YadoScript : MonoBehaviour
 
     GameObject childObj;
     private bool isHold;
+    public float[] levelUpTime;
+    private float xpCount;
+    private int levelCount;//現在のレベル
 
     public void SetIsHold(bool set) { isHold = set; }
     void Start()
     {
         childObj = transform.GetChild(0).gameObject;
+        levelCount = 0;
     }
 
     // Update is called once per frame
@@ -20,6 +24,21 @@ public class YadoScript : MonoBehaviour
     {
         if (isHold)
         {
+            if (levelCount != levelUpTime.Length) {
+                xpCount += Time.deltaTime;
+                //ヤドの経験値がレベル区分の量に達したらレベルアップする
+                if (xpCount >= levelUpTime[levelCount])
+                {
+                    xpCount = 0;
+                    levelCount++;
+                    Debug.Log("nowLevel=" + levelCount);
+                }
+            }
+            
+
+           
+
+
             childObj.SetActive(false);
         }
         else
